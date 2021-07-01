bd81e2e9f1a5dd7b7644a41254fca84560f46b34
Khoren Antonyan <khorenantonian@gmail.com>
2021-07-01 23:57:27 +0300

    Add project files.

---
+++ DotNetBookstore.API/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetBookstore.Infrastructure.Data;
+using DotNetBookstore.Core.Entities;
+
+namespace DotNetBookstore.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Book>>> GetProducts()
+        {
+            var books = await _context.Books.ToListAsync();
+
+            return Ok(books);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetProduct(int id)
+        {
+            return await _context.Books.FindAsync(id);
+        }
+    }
+}
---
+++ Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using DotNetBookstore.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetBookstore.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
